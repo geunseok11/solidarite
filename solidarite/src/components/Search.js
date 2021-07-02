275b324966623cef450c9d8a93961055018df0d7
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-07-02 12:01:42 +0900

    Add components

---
+++ solidarite/src/components/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <figure>
+        <input placeholder="검색어를 입력해주세요"></input>
+      </figure>
+    </div>
+  );
+};
+
+export default Search;
